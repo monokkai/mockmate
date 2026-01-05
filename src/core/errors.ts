e41cb61a9475ddb51e69fc3b43c1e1c724d4c241
monokkai <egormayer08@mail.ru>
2026-01-06 00:05:40 +0100

    feat: added error handler

---
+++ src/core/errors.ts
@@ -0,0 +1,13 @@
+export class MockmateError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "MockmateError"
+    }
+}
+
+export class SourceFetchError extends MockmateError {
+    constructor(message: string) {
+        super(message);
+        this.name = "SourceFetchError"
+    }
+}
