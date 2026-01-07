0d02426662c57ee699b5cc7ddba0b758646b9bd2
monokkai <egormayer08@mail.ru>
2026-01-07 21:48:29 +0100

    feat: added schemas, ability to extend json response

---
+++ src/core/shema.ts
@@ -0,0 +1,17 @@
+import type { SchemaDefenititon } from "./types";
+
+export function generateFromSchema(schema: SchemaDefenititon, quantity: number = 1) {
+    const result = [];
+
+    for (let i = 0; i < quantity; i++) {
+        const item: Record<string, unknown> = {};
+
+        for (const key in schema) {
+            item[key] = schema[key]();
+        }
+
+        result.push(item)
+    }
+
+    return result;
+}
