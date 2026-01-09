7dd19b464ed2460d4d577c0c4bd9dd663c54bbdd
monokkai <egormayer08@mail.ru>
2026-01-09 21:47:33 +0100

    dev: minor update of files

--- src/examples/generate.users.ts
+++ src/examples/generate.users.ts
@@ -4,9 +4,6 @@ async function run() {
   const users = await mockmate({
     category: "users",
     quantity: 2,
-    extend: {
-      zipcode: () => "90566 - 7771",
-    },
   });
 
   console.groupCollapsed("Generated users: ");
